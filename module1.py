779ccca201faa56425547af1b4d618f1fa8f576e
Sareff <ilbezr@gmail.com>
2022-03-12 22:48:47 +0300

    Moddified command parser to handle length and parent args. Also make some changes in MathSunset init process to work as I want.

--- module1.py
+++ module1.py
@@ -34,9 +34,36 @@ class MathSubset(MathSet):
   - parent: This field contains a reference to the set from which this subset originated.
 
   """
-  def __init__(self, parent):
-    super().__init__()
+  def __init__(self, name, inf, parent, inner):
     self.parent = parent
+    super(MathSubset, self).__init__(name, inf, inner)
+
+
+def findString(arr, name):
+  """
+  This method allow us to find string in array even if this string have any other characters around.
+  """
+  return [s for s in arr if name in s]
+
+def generateName(pattern):
+  """
+  This method will generate a name for object and store this name in global name heap to avoid duplication.
+  """
+  global nameHeap
+  global nameIndex
+  name = pattern + str(nameIndex)
+  nameIndex += 1
+  return name
+
+
+def safeCast(value, toType, default=None):
+  """
+  safeCast is a method to safely cast any value to any type, wether it is string to int casring or something else.
+  """
+  try:
+    return toType(value)
+  except (ValueError, TypeError):
+    return default
 
 def createSchema(new):
   """
@@ -50,14 +77,56 @@ def createSchema(new):
   """
   new = new.split()
   command = new[0]
-  matchName = [s for s in new if "name" in s]
-  matchInf = [s for s in new if "inf" in s]
-  matchParent = [s for s in new if "isinstanceof" in s]
-
   value = new[-1]
+
+  # Searching for arguments.
+  # If argument is represented in command, the value of match* will be empty list [].
+  matchName = findString(new, "name") 
+  matchInf = findString(new, "inf") 
+  matchParent = findString(new, "isinstanceof")
+  matchLength = findString(new, "length")
+
+  #Making empty arguments to have default values.
+  if len(matchName) < 1:
+    match command:
+      case "create-set":
+        namePattern = "S"
+      case "create-subset":
+        namePattern = "SubS"
+      case "create-pair":
+        namePattern = "P"
+      case "create-sortset":
+        namePattern = "SS"
+      case _:
+        namePattern = "Obj"
+    matchName = generateName(namePattern)
+  else:
+    matchName = matchName[0].split("=")[1]
+
+  if len(matchInf) < 1:
+    matchInf = False
+  elif len(matchInf) > 1 and matchInf[0].split("=")[1] in ("1", "True", "true"):
+    matchInf = True
+  else:
+    matchInf = False
+
+  if len(matchParent) < 1:
+    matchParent = ""
+  else:
+    matchParent = matchParent[0].split("=")[1]
+
+  if len(matchLength) < 1:
+    matchLength = ""
+  else:
+    matchLength = safeCast(matchLength[0].split("=")[1], int, "") 
   
-  return {"command": command, "name": matchName[0].split("=")[1] if len(matchName) > 0 else "S1",
-          "infinity": matchInf[0].split("=")[1] if (len(matchInf) > 0) else "0", "value": value}
+  schematic =  {"command": command, 
+                "name": matchName,
+                "infinity": matchInf, 
+                "parent": matchParent,
+                "length": matchLength,
+                "value": value}
+  return schematic
 
 def readCommand(new):
   global ObjList
@@ -67,16 +136,17 @@ def readCommand(new):
     return "Command should use arguments"
 
   schematics = createSchema(new)
+  name = schematics["name"]
 
   match schematics["command"]:
     case "create-set":
-      name = schematics["name"]
       ObjList[name] = MathSet(name, schematics["infinity"], schematics["value"])
-      return ObjList 
+      return (ObjList, schematics) 
     case "create-subset":
-      name = schematics["name"]
-      ObjList[name] = MathSubset(name, schematics["infinity"], schematics["value"])
-      return ObjList 
+      ObjList[name] = MathSubset(name, schematics["infinity"], schematics["parent"], schematics["value"])
+      return (ObjList, schematics)
+    case "create-sortset":
+      pass
     case _:
       return "No such command"
 
@@ -98,6 +168,8 @@ def readCommand(new):
 if __name__ == "__main__":
 
   ObjList = dict()
+  nameHeap = []
+  nameIndex = 0
   acceptedCommands = ("create-set", "create-subset", "create-pair", "create-sortset")
   while (b := input("[CMD] ")) != "exit":
     print(readCommand(b))
